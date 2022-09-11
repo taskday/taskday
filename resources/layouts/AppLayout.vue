3691a6c0d2f9be1a62aca1873c6531e68c1603d1
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-09-11 12:15:43 +0200

    Update app layout

--- resources/layouts/AppLayout.vue
+++ resources/layouts/AppLayout.vue
@@ -1,28 +1,4 @@
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-100">
-    <body class="h-full">
-    ```
-  -->
   <div>
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog
@@ -84,24 +60,24 @@
               </div>
               <div class="mt-5 h-0 flex-1 overflow-y-auto">
                 <nav class="space-y-1 px-2">
-                  <a
+                  <Link
                     v-for="item in navigation"
                     :key="item.name"
                     :href="item.href"
                     :class="[
-                      item.current
+                      item.current()
                         ? 'bg-indigo-800 text-white'
                         : 'text-indigo-100 hover:bg-indigo-600',
                       'group flex items-center px-2 py-2 text-base font-medium rounded-md',
                     ]"
                   >
-                    <component
-                      :is="item.icon"
+                    <v-icon
+                      :name="item.icon"
                       class="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300"
                       aria-hidden="true"
                     />
                     {{ item.name }}
-                  </a>
+                  </Link>
                 </nav>
               </div>
             </DialogPanel>
@@ -117,33 +93,29 @@
     <div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex flex-grow flex-col overflow-y-auto bg-indigo-700 pt-5">
-        <div class="flex flex-shrink-0 items-center px-4">
-          <img
-            class="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
-            alt="Your Company"
-          />
+        <div class="flex flex-shrink-0 items-center px-4 text-white font-bold">
+          Taskday
         </div>
         <div class="mt-5 flex flex-1 flex-col">
           <nav class="flex-1 space-y-1 px-2 pb-4">
-            <a
+            <Link
               v-for="item in navigation"
               :key="item.name"
               :href="item.href"
               :class="[
-                item.current
+                item.current()
                   ? 'bg-indigo-800 text-white'
                   : 'text-indigo-100 hover:bg-indigo-600',
                 'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
               ]"
             >
-              <component
-                :is="item.icon"
+              <v-icon
+                :name="item.icon"
                 class="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300"
                 aria-hidden="true"
               />
               {{ item.name }}
-            </a>
+            </Link>
           </nav>
         </div>
       </div>
@@ -156,7 +128,7 @@
           @click="sidebarOpen = true"
         >
           <span class="sr-only">Open sidebar</span>
-          <Bars3BottomLeftIcon class="h-6 w-6" aria-hidden="true" />
+          <v-icon name="menu" class="h-6 w-6" aria-hidden="true" />
         </button>
         <div class="flex flex-1 justify-between px-4">
           <div class="flex flex-1">
@@ -168,7 +140,7 @@
                 <div
                   class="pointer-events-none absolute inset-y-0 left-0 flex items-center"
                 >
-                  <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
+                  <v-icon name="search" class="h-5 w-5" aria-hidden="true" />
                 </div>
                 <input
                   id="search-field"
@@ -186,7 +158,7 @@
               class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
+              <v-icon name="bell" class="h-6 w-6" aria-hidden="true" />
             </button>
 
             <!-- Profile dropdown -->
@@ -229,7 +201,7 @@
                       {{ item.name }}
                     </Link>
                   </MenuItem>
-                  <hr>
+                  <hr />
                   <MenuItem
                     v-for="item in extraNavigation"
                     :key="item.name"
@@ -255,17 +227,13 @@
 
       <main>
         <div class="py-6">
-          <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+          <div class="mx-auto container px-4 sm:px-6 md:px-8 mb-8">
+            <h1 class="text-2xl font-semibold text-gray-900">
+              {{ $page.props.title }}
+            </h1>
           </div>
-          <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <!-- Replace with your content -->
-            <div class="py-4">
-              <div
-                class="h-96 rounded-lg border-4 border-dashed border-gray-200"
-              />
-            </div>
-            <!-- /End replace -->
+          <div class="mx-auto container px-4 sm:px-6 md:px-8">
+            <slot></slot>
           </div>
         </div>
       </main>
@@ -287,21 +255,23 @@ import {
 } from "@headlessui/vue";
 
 const navigation = [
-  { name: "Dashboard", href: "#", icon: 'HomeIcon', current: true },
-  { name: "Team", href: "#", icon: 'UsersIcon', current: false },
-  { name: "Projects", href: "#", icon: 'FolderIcon', current: false },
-  { name: "Calendar", href: "#", icon: 'CalendarIcon', current: false },
-  { name: "Documents", href: "#", icon: 'InboxIcon', current: false },
-  { name: "Reports", href: "#", icon: 'ChartBarIcon', current: false },
+  {
+    name: "Dashboard",
+    href: route("dashboard"),
+    icon: "home",
+    current: () => route().current() == "dashboard",
+  },
+  {
+    name: "All Entries",
+    href: route("entries.index"),
+    icon: "list",
+    current: () => route().current() == "entries.index",
+  },
 ];
 
-const userNavigation = [
-  { name: "Account", href: '#' },
-];
+const userNavigation = [{ name: "Account", href: "#" }];
 
-const extraNavigation = [
-  { name: "Sign out", href: route("logout") },
-];
+const extraNavigation = [{ name: "Sign out", href: route("logout") }];
 
 const sidebarOpen = ref(false);
 </script>
