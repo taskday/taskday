c78d4727e43eb1e8d072837bc0bc4e8ef36ee372
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-09-11 12:15:54 +0200

    Update entries index view

--- resources/pages/Entries/Index.vue
+++ resources/pages/Entries/Index.vue
@@ -1,10 +1,34 @@
 <script lang="ts" setup>
-import { ref, reactive, onMounted, watch } from 'vue';
+import { PropType } from "vue";
+import { useResource } from "@/composables/useResource";
 
+let props = defineProps({
+  entries: {
+    type: Object as PropType<Pagination<Entry>>,
+    required: true,
+  },
+});
+
+const { data, loading, disabled, loadMore } = useResource<Entry>(
+  route("api.entries.index"),
+  props.entries
+);
 </script>
 
 <template>
-  <div>
-
+  <div class="space-y-4">
+    <div v-for="entry in data.data" :key="entry.id">
+      <Link class="hover:underline" :href="route('entries.show', entry)">
+        {{ entry.title }}
+      </Link>
+    </div>
+    <v-button
+      @click="loadMore"
+      :disabled="disabled"
+      :loading="loading"
+      class="button-primary"
+    >
+      Load more
+    </v-button>
   </div>
 </template>
